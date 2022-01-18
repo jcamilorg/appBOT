ee1b3520eb7a16fe6f735a50741c5226903efd89
Juan Camilo Ramirez <juancamilorg64@gmail.com>
2022-01-18 08:13:05 -0500

    Version 0.0.2 se empezo a trabajar en la interfaz grafica

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 //
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>BOT ("be on time app")</Text>
-      <Text>Never miss a task</Text>
-      <Text>by. Juan Camilo Ramirez, Sergio Guyumus, Jefferson Hernandez</Text>
-      <StatusBar style="auto" />
+      <View style ={styles.header} >
+        <Text style={styles.title}>BOT</Text>
+        <Text>Never miss a task</Text>
+      </View>
+      <View style={styles.body}>
+        <View style ={styles.containerDays}>
+          <Text style={styles.days}>Lunes</Text>
+          <Text style={styles.days}>Martes</Text>
+        </View>
+        <View style ={styles.containerDays}>
+          <Text>Miercoles</Text>
+          <Text>Jueves</Text>
+        </View>
+        <View style ={styles.containerDays}>
+          <Text>Viernes</Text>
+          <Text>Sabado</Text>
+        </View>
+        <View style ={styles.containerDays}>
+          <Text>Domingo</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -15,8 +33,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+
+    
   },
+
+  body: {
+    flex:9,
+    backgroundColor: '#ffd',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  header: {
+    flex: 1,
+    backgroundColor: '#fcba03',
+    alignItems: 'center',
+    fontWeight: "bold",
+    paddingTop: 30,
+
+  },
+
+  title: {
+    fontSize: 60,
+    color: '#fff',
+    fontWeight: 'bold',
+    letterSpacing: 5,
+  },
+
+ containerDays : {
+   flex: 1,
+   margin: 10,
+   paddingTop: 50,
+   backgroundColor: '#faa',
+   flexDirection: 'row',
+ },
+
+days: {
+  flex:1,
+  margin: 5,
+  backgroundColor: "#0ff"
+},
+
 });
